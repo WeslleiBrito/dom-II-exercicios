a8664944b24f59eb76f87dcf6b208c09a41c1da5
WeslleiBrito <wesllei.wbs@gmail.com>
2023-01-19 20:52:30 -0300

    publicacao no surge

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function adicionaItem(event) {
     event.preventDefault()
     const container = document.getElementById('container')
+
     const novoElemento = document.createElement('article')
     novoElemento.setAttribute('class', 'item')
     novoElemento.setAttribute('onclick', 'removeItem(event)')
@@ -11,8 +12,7 @@ function adicionaItem(event) {
 
 
 function removeItem(event) {
-    /* O event.target devolve o elemento que gerou o evento */
-    const elemento = event.target
-        /* Quando especifico o "elemento" só ele será removido, mesmo exitindo vários elementos com a mesma classe que ele */
-    elemento.classList.remove("item")
+    /* O event.target devolve o elemento que gerou o evento, e nele mesmo eu executo a função de remover */
+    event.target.remove()
+
 }
